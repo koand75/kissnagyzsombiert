0317196989d61d7f530160c469839fd12e5ef743
koand <kovacs.andrea@nexius.hu>
2026-01-05 21:24:07 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { News } from './pages/news/news';
+import { Donation } from './pages/donation/donation';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'news', component: News},
+    { path: 'donation', component: Donation },
+    { path: 'contact', component: Contact}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
+  sidebarOpen = window.innerWidth > 768;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
   protected readonly title = signal('kissnagyzsombiert');
+
+  closeSidebarOnMobile() {
+  if (window.innerWidth <= 768) {
+    this.sidebarOpen = false;
+  }
+}
 }
