73268b029afbae3124dfde5b4b994d6327ca76f0
koand <kovacs.andrea@nexius.hu>
2026-01-08 09:06:21 +0100

    Add copy to clipboard donation data

--- src/app/pages/donation/donation.ts
+++ src/app/pages/donation/donation.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './donation.html',
   styleUrl: './donation.scss',
 })
-export class Donation {
 
+export class Donation {
+  donationData = {
+    name: 'Kiss-Nagy Zsomborért Alapítvány',
+    registrationNumber: '10-01-0001211',
+    taxNumber: '19288026-1-10',
+    accountNumber: '10403538-50526980-72761011',
+    iban: 'HU20 10403538 50526980 72761011',
+    swift: 'OKHBHUHB',
+    bank: 'K&H Bank'
+  };
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text);
+  }
 }
