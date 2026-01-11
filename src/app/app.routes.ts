4e377202d1684615f0b60b943aae2759fbee30f2
koand <kovacs.andrea@nexius.hu>
2026-01-11 23:10:39 +0100

    Add events page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { News } from './pages/news/news';
 import { Donation } from './pages/donation/donation';
+import { Events } from './pages/events/events';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'news', component: News},
+    { path: 'news', component: News },
     { path: 'donation', component: Donation },
-    { path: 'contact', component: Contact}
+    { path: 'events', component: Events },
+    { path: 'contact', component: Contact }
 ];
